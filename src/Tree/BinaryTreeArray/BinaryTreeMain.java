e273e5024bb8566725ae044a0f64f71ce50f6743
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-12-12 09:15:28 +0530

    09 commit

--- src/Tree/BinaryTreeArray/BinaryTreeMain.java
+++ src/Tree/BinaryTreeArray/BinaryTreeMain.java
@@ -2,7 +2,7 @@ package Tree.BinaryTreeArray;
 
 public class BinaryTreeMain {
     public static void main(String[] args) {
-        BinaryTreeArray tree=new BinaryTreeArray(5);
+        BinaryTreeArray tree=new BinaryTreeArray(9);
 
         tree.insert("N1");
         tree.insert("N2");
@@ -10,6 +10,12 @@ public class BinaryTreeMain {
         tree.insert("N4");
         tree.insert("N5");
         tree.insert("N6");
+        tree.insert("N7");
+        tree.insert("N8");
+        tree.insert("N9");
+        System.out.println(" ");
+        tree.delete("N3");
+        tree.levelOrder();
     }
 
 
