3be620e35a43e22488fd904cb8eec173dbdf6530
choubeyshubham <shubhamchoubey96@gmail.com>
2025-03-06 18:25:27 +0530

    11 commit

---
+++ src/BigONotation/Introduction.java
@@ -0,0 +1,37 @@
+package BigONotation;
+
+public class Introduction {
+
+    /*
+
+    Big O Notations is the language and metric we use
+    to describe the efficiency of algorithms
+
+
+    Best Case
+    Average Case
+    Worst Case
+
+
+    O(1) Constant
+    O(N) Linear
+    O(LogN) Logarithmic
+    O(N2) Quadratic
+    O(N3) cube time complexity
+
+
+
+
+
+
+
+
+
+
+
+     */
+
+
+
+
+}
---
+++ src/Introduction/Introduction.java
@@ -0,0 +1,51 @@
+package Introduction;
+
+public class Introduction {
+
+/*
+Data structure are a different way of storing data
+on your computer
+Organizing data so it can be used efficiently
+
+
+Set of instruction to perform a task
+
+
+types of data structure
+
+Primitive
+ Integer
+ Float
+ Character
+ Boolean
+
+Non-Primitive
+  Linear
+    Static
+        Array
+    Dynamic
+        LinkedList
+        Stack
+        Queue
+  Non Linear
+    Tree
+    Graph
+=========================
+Types of Algorithm
+    Simple recursive algorithm
+    Divide and conquer algorithms
+    Dynamic programming algorithms
+    Greedy algorithms
+    brute force algorithms
+    Randomized algorithms
+
+
+
+
+ */
+
+
+
+
+
+}
