6ad9a907d4ae6f34bd536558de612666782861c3
choubeyshubham <shubhamchoubey96@gmail.com>
2025-03-07 12:28:08 +0530

    13 commit

---
+++ src/Arrays/Arrayss.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Arrayss {
+
+    /*
+
+
+
+     */
+
+    public static void main(String[] args) {
+
+        int[] arr;//O(1)
+        arr= new int[3];//O(1)
+        arr[0]=1;//O(1)
+        arr[1]=2;//O(1)            combine O(n)
+        arr[2]=3;//O(1)
+
+        System.out.println(Arrays.toString(arr));
+
+        String[] allArr={"a","b","c"};
+        System.out.println(Arrays.toString(allArr));
+
+
+
+    }
+
+
+}
