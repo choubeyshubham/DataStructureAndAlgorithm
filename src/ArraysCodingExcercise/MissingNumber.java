c916a5e31d0110c16dee05436e4c3c1d19c876a2
choubeyshubham <shubhamchoubey96@gmail.com>
2025-03-15 12:47:46 +0530

    18 commit array coding 2

---
+++ src/ArraysCodingExcercise/MissingNumber.java
@@ -0,0 +1,36 @@
+package ArraysCodingExcercise;
+
+public class MissingNumber {
+    /*
+Write Java function called findMissingNumberInArray that takes an integer array containing n-1 unique elements from a range of 1 to n, with one missing number, and returns the missing number.
+
+Example
+
+myArray = {1,2,3,4,6}
+findMissingNumberInArray(myArray, 6) // 5
+Hint:
+
+Use the formula (n * (n + 1)) / 2 which  calculates the sum of the first n natural numbers.
+
+
+     */
+
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,4,6};
+        System.out.println(findMissingNumberInArray(myArray));
+
+    }
+
+    public static int findMissingNumberInArray(int[] arr) {
+        int n= arr.length+1;
+        int sum=(n*(n+1))/2;
+        int acSum=0;
+        for (int s : arr) {
+            acSum+=s;
+        }
+        int res=sum-acSum;
+        return res;
+    }
+
+
+}
---
+++ src/ArraysCodingExcercise/RemoveDuplicate.java
@@ -0,0 +1,47 @@
+package ArraysCodingExcercise;
+
+import java.util.Arrays;
+
+public class RemoveDuplicate {
+    /*
+Write a function which takes integer array as a parameter and returns a new integer array with unique elements. (remove duplicates)
+
+Example
+
+Output : [1, 2, 3, 4, 5]
+
+     */
+
+    public static void main(String[] args) {
+        int[] arr={1, 1, 2, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(removeDuplicates(arr)));
+    }
+
+
+    public static int[] removeDuplicates(int[] arr) {
+       int n = arr.length;
+       int[] temp = new int[n];
+       int index=0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isDup=false;
+            for (int j = i+1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    isDup=true;
+                    break;
+                }
+            }
+            if (!isDup) {
+                temp[index++]=arr[i];
+            }
+
+        }
+
+        return Arrays.copyOf(temp, index);
+
+    }
+
+}
+
+
