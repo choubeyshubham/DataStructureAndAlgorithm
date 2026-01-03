69f8eec8f81079b09fdbeab12e4f9905c22b2882
choubeyshubham <shubhamchoubey96@gmail.com>
2026-01-03 16:32:14 +0530

    41 commit

--- src/DS/DynamicProgramming/Main.java
+++ src/DS/DynamicProgramming/Main.java
@@ -8,37 +8,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DPMemo dpMemo = new DPMemo();
     HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
-//    System.out.println(fibMemo(6,map));
-    System.out.println(fibTab(6));
+    System.out.println(dpMemo.fibMemo(6,map));
+    System.out.println(dpMemo.fibTab(6));
+
+
+// ---------------Number Factor
+
+    NumberOfFactor np=new NumberOfFactor();
+        System.out.println(np.numberFactorTopDown(4));
+
+        System.out.println(np.numberFactorDownTop(4));
 
 
     }
 
-    public static int fibMemo(int n, HashMap<Integer,Integer> memo){
-        if (n==1) return 0;
-        if(n==2) return 1;
-        if(!memo.containsKey(n)){
-            memo.put(n,(fibMemo(n-1,memo)+fibMemo(n-2,memo)));
-        }
-        return memo.get(n);
 
-    }
 
-    // Tabulation
-    public static int fibTab(int n){
-        ArrayList<Integer> tb=new ArrayList<Integer>();
-        tb.add(0);
-        tb.add(1);
-        for (int i=2;i<=n-1;i++){
-            int n1 = tb.get(i-1);
-            int n2 = tb.get(i-2);
-            tb.add(n1+n2);
-        }
-        return tb.get(n-1);
-    }
-
-    //
 
 
 
--- src/DS/DynamicProgramming/NumberOfFactor.java
+++ src/DS/DynamicProgramming/NumberOfFactor.java
@@ -3,21 +3,34 @@ package DS.DynamicProgramming;
 public class NumberOfFactor {
 
 
-    public int numberFactor(int n,int[] dp){
+    public int numberFactorTopDown(int[] dp,int n){
     if(n==0 || n==1 || n==2) return 1;
     if(n==3) return 2;
-    for (int d :dp){
-        if(n==d) return dp[n];
+
+    if(dp[0]==0) {
+        int sp1=numberFactorTopDown(dp,n-1);
+        int sp2=numberFactorTopDown(dp,n-3);
+        int sp3=numberFactorTopDown(dp,n-4);
+        dp[n]=sp1+sp2+sp3;
     }
 
-
-        int rec1=numberFactor(n-1,dp);
-        int rec2=numberFactor(n-3,dp);
-        int rec3=numberFactor(n-4,dp);
-        dp[n]=rec1+rec2+rec3;
         return dp[n];
+    }
 
+    public int numberFactorTopDown(int n){
+        int[] dp= new int[n+1];
+        return numberFactorTopDown(dp,n);
 
+    }
+
+    public int numberFactorDownTop(int n){
+        int[] dp = new int[n+1];
+        dp[0]=dp[1]=dp[2]=1;
+        dp[3]=2;
+        for(int i=4;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-3]+dp[i-4];
+        }
+        return dp[n];
     }
 
 
